463293c733067a13d116b9f9e7c9f1630bf8d3dc
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2019-08-24 06:53:13 +0200

    move list items

--- src/app/page-todo/page-todo.component.ts
+++ src/app/page-todo/page-todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import TodoItem from "../../models/TodoItem";
 
 @Component({
   selector: 'app-page-todo',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageTodoComponent implements OnInit {
 
+  listLeft = [new TodoItem(0, 'txt1'), new TodoItem(1, 'txt2'),new TodoItem(2, 'txt3'), new TodoItem(3, 'txt4')]
+  listRight = []
+
   constructor() { }
 
   ngOnInit() {
+    // console.log(this.listLeft, 'this.listLeft')
+    // this.listLeft[0].id
+    // console.log(this.listLeft, 'this.listLeft')
+  }
+
+  btnLeftListItemClick(id: number) {
+    console.log(id, 'byn click')
+    const itemFound = this.listLeft.find((v: any, i: number) => v.id === id)
+    this.listRight.push(itemFound)
+    this.listLeft = this.listLeft.filter(v => v.id !== id)
+  }
+
+  btnRightListItemClick(id: number) {
+    console.log(id, 'byn click')
+    const itemFound = this.listRight.find((v: any, i: number) => v.id === id)
+    this.listLeft.push(itemFound)
+    this.listRight = this.listRight.filter(v => v.id !== id)
   }
 
 }
---
+++ src/models/TodoItem.ts
@@ -0,0 +1,6 @@
+export default class TodoItem {
+    // id: number = null
+    // textVal: string = ''
+
+    constructor (public id: number = 0, public textVal: string = '') {}
+}
