56623a4184cf4698b2ccad3c9d6c4aeea105aa54
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2019-09-01 19:16:09 +0200

    todo reducer  conect to ngrx

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { increment, decrement, reset } from '../stateMan/actions/counter.actions';
+import { selectData, TodoState, todoReducer } from '../stateMan/reducer/todo.reducer'
+import TodoItem from '../../models/TodoItem'
 
 @Component({
   selector: 'app-counter',
@@ -10,13 +12,23 @@ import { increment, decrement, reset } from '../stateMan/actions/counter.actions
 })
 export class CounterComponent implements OnInit {
   count$: Observable<number>;
+  data$: Observable<TodoState>;
+  listLeft$ : Observable<TodoItem[]>;
 
-  constructor(private store: Store<{ count: number }>) {
+  constructor(private store: Store<{count: number, todo: TodoState}>) {
     this.count$ = store.pipe(select('count'));
-    console.log('store', store)
+    // console.log('store', store)
+    // console.log('selectData', selectData(store))
+    // console.log('selectListLeft', selectListLeft)
+    // this.leftList$ = this.store.select('data')
   }
   
   ngOnInit() {
+    this.data$ = this.store.select('todo')
+    this.listLeft$ = this.store.select('todo', 'data', 'leftList$')
+
+    // show the value of the item in the state
+    this.data$.subscribe((x: TodoState) => console.log('this.data$', x.data.listLeft))
   }
 
   increment() {
